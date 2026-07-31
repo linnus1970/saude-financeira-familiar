83df1f4d28ba08b9f3d76f17d476da02d856fec2
linnus1970 <lineiarnhold@gmail.com>
2026-07-31 20:37:49 -0300

    Sprint 2 - autenticacao Firebase completa

--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -1,34 +1,10 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+
+import '../features/auth/presentation/pages/auth_flow_page.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const HomePage(),
-    ),
+    GoRoute(path: '/', builder: (context, state) => const AuthFlowPage()),
   ],
 );
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Saúde Financeira Familiar'),
-      ),
-      body: const Center(
-        child: Text(
-          'Sprint 1 funcionando!',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  }
-}
